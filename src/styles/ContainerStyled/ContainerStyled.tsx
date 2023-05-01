5d72a1cbf5bb053a3a02dbbd7bf1d68fcdef6599
Constantin Dusescu <constantin.dusescu@icloud.com>
2023-05-01 23:05:42 +0200

    Add main centered container

---
+++ src/styles/ContainerStyled/ContainerStyled.tsx
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+const ContainerStyled = styled.div`
+  max-width: 1000px;
+  padding-inline: ${(props) => props.theme.spacing.medium};
+  margin-inline: auto;
+`;
+
+export default ContainerStyled;
