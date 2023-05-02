e729d38484451204ec4dd6838a72da235e770a46
Constantin Dusescu <constantin.dusescu@icloud.com>
2023-05-02 23:32:06 +0200

    Fix loadGameCardsActionCreator test

--- src/store/actions/gameCards/gameCardsActionCreators.test.ts
+++ src/store/actions/gameCards/gameCardsActionCreators.test.ts
@@ -1,43 +1,42 @@
 import { loadGameCardsActionCreator } from "./gameCardsActionCreators";
 import { GameCardsActionsType } from "./types";
 
-const gameCards = [
-  {
-    id: 2,
-    name: "Fencing Ace",
-    type: "Creature — Human Soldier",
-    flavor:
-      "His prowess gives the guildless hope that they can hold out against tyranny.",
-    rarity: "Uncommon",
-    artist: "David Rapoza",
-    imageUrl:
-      "http://gatherer.wizards.com/Handlers/Image.ashx?multiverseid=489688&type=card",
-    number: 2,
-  },
-  {
-    id: 3,
-    name: "Flickerwisp",
-    type: "Creature — Elemental",
-    flavor: "Its wings disturb more than air.",
-    rarity: "Uncommon",
-    artist: "Jeremy Enecio",
-    imageUrl:
-      "http://gatherer.wizards.com/Handlers/Image.ashx?multiverseid=489689&type=card",
-    number: 3,
-  },
-];
-
 describe("Given a function loadGameCardsActionCreator", () => {
-  describe("When it receives gameCards", () => {
-    test("Then it should return an action object with type: loadCharacters and payload: gameCards", () => {
-      const action = {
-        type: GameCardsActionsType.loadCharacters,
-        payload: gameCards,
-      };
+  describe("When it receives a list with Fencing Ace and Flickerwisp", () => {
+    test("Then it should return an load game cards action with Fencing Ace and Flickerwisp as payload", () => {
+      const gameCards = [
+        {
+          id: 2,
+          name: "Fencing Ace",
+          type: "Creature — Human Soldier",
+          flavor:
+            "His prowess gives the guildless hope that they can hold out against tyranny.",
+          rarity: "Uncommon",
+          artist: "David Rapoza",
+          imageUrl:
+            "http://gatherer.wizards.com/Handlers/Image.ashx?multiverseid=489688&type=card",
+          number: 2,
+        },
+        {
+          id: 3,
+          name: "Flickerwisp",
+          type: "Creature — Elemental",
+          flavor: "Its wings disturb more than air.",
+          rarity: "Uncommon",
+          artist: "Jeremy Enecio",
+          imageUrl:
+            "http://gatherer.wizards.com/Handlers/Image.ashx?multiverseid=489689&type=card",
+          number: 3,
+        },
+      ];
 
-      const expectedAction = loadGameCardsActionCreator(gameCards);
+      const loadGameCardsAction = loadGameCardsActionCreator(gameCards);
 
-      expect(expectedAction).toStrictEqual(action);
+      expect(loadGameCardsAction).toHaveProperty("payload", gameCards);
+      expect(loadGameCardsAction).toHaveProperty(
+        "type",
+        GameCardsActionsType.loadCharacters
+      );
     });
   });
 });
