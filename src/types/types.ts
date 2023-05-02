1673e5980f10f40f2e43966c69e34272391996fc
Pol <polgil24@hotmail.com>
2023-05-02 09:48:27 +0200

    Add CardStructure interface

---
+++ src/types/types.ts
@@ -0,0 +1,10 @@
+export interface CardStructure {
+  name: string;
+  type: string;
+  flavor: string;
+  rarity: string;
+  artist: string;
+  imageUrl: string;
+  id: number;
+  number: number;
+}
