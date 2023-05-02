081d45dbc4b6fe5ce30a54b4ff4051a4e84e706d
bboyruso <bboyruso@hotmail.com>
2023-05-02 12:24:40 -0700

    Add NavBarComponent

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,14 +1,11 @@
 import LayoutStyled from "../Layout/LayoutStyled";
 import Layout from "../Layout/Layout";
-import Navbar from "../Navbar/Navbar";
 
 const App = (): JSX.Element => {
   return (
-
     <LayoutStyled>
       <Layout />
     </LayoutStyled>
-
   );
 };
 
