4fb6de90b32dabd42bdb3c95f3116d94c3ae5c87
Sergey Shaposhnik <bboyruso@hotmail.com>
2023-05-03 13:40:34 +0200

    Add Button component and ButtonStyled

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,12 +1,10 @@
 import LayoutStyled from "../Layout/LayoutStyled";
 import Layout from "../Layout/Layout";
-import Header from "../Header/Header";
 
 const App = (): JSX.Element => {
   return (
     <LayoutStyled>
       <Layout />
-      <Header />
     </LayoutStyled>
   );
 };
---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,7 @@
+import ButtonStyled from "./ButtonStyled";
+
+const Button = (text: string): JSX.Element => {
+  return <ButtonStyled>{text}</ButtonStyled>;
+};
+
+export default Button;
