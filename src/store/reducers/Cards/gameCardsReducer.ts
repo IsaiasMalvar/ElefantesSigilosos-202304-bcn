9c6e1e466e7a5b228243f6c066e6830dfb55a146
Constantin Dusescu <constantin.dusescu@icloud.com>
2023-05-03 19:38:26 +0200

    Change switch in gameCardsReducer to use if conditional

--- src/store/reducers/Cards/gameCardsReducer.ts
+++ src/store/reducers/Cards/gameCardsReducer.ts
@@ -2,32 +2,25 @@ import { Reducer } from "react";
 import {
   GameCardsActionsType,
   LoadGameCardsAction,
+  GameCardsAction,
 } from "../../actions/gameCards/types";
-import { GameCardsAction } from "../../actions/gameCards/types";
 import { GameCardsState } from "../../contexts/GameCardsContext/types";
 
 const gameCardsReducer: Reducer<GameCardsState, GameCardsAction> = (
   currentCardStore,
   action
 ): GameCardsState => {
-  let newCardStore: GameCardsState = {
-    gameCards: [],
-    currentPage: 1,
-  };
-  switch (action.type) {
-    case GameCardsActionsType.loadGameCards:
-      newCardStore = {
-        gameCards: [
-          ...currentCardStore.gameCards,
-          ...(action as LoadGameCardsAction).payload,
-        ],
-        currentPage: currentCardStore.currentPage,
-      };
-      break;
-    default:
-      newCardStore = currentCardStore;
+  if (action.type === GameCardsActionsType.loadGameCards) {
+    return {
+      gameCards: [
+        ...currentCardStore.gameCards,
+        ...(action as LoadGameCardsAction).payload,
+      ],
+      currentPage: currentCardStore.currentPage,
+    };
   }
-  return newCardStore;
+
+  return currentCardStore;
 };
 
 export default gameCardsReducer;
