6349d00fa829d61d3674a777f9809a4e7be39cac
Constantin Dusescu <constantin.dusescu@icloud.com>
2023-05-03 10:20:57 +0200

    Add CardsListPage and styles

---
+++ src/pages/CardsListPage/CardsListPage.tsx
@@ -0,0 +1,11 @@
+import CardsListPageStyled from "./CardsListPageStyled";
+
+const CardsPage = () => {
+  return (
+    <CardsListPageStyled>
+      <h1 className="cards-intro">Explore our cards</h1>
+    </CardsListPageStyled>
+  );
+};
+
+export default CardsPage;
--- src/routers/appRouter/appRouter.tsx
+++ src/routers/appRouter/appRouter.tsx
@@ -1,10 +1,17 @@
 import { RouteObject, createBrowserRouter } from "react-router-dom";
 import App from "../../components/App/App";
+import CardsPage from "../../pages/CardsListPage/CardsListPage";
 
 const routes: RouteObject[] = [
   {
     path: "/",
     element: <App />,
+    children: [
+      {
+        path: "cards",
+        element: <CardsPage />,
+      },
+    ],
   },
 ];
 
